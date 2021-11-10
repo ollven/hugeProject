6186e5a21395f8e528bf84fae397a9f71b234141
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-11-10 11:46:54 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,20 +4,5 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 version = "2021.2"
 
 project {
-    val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
-    val numConfigurationsPerProject = Integer.parseInt(DslContext.getParameter("numConfigurationsPerProject"))
 
-    for (i in 0..numProjects) {
-        subProject {
-            id("subProj_$i")
-            name = "subProj $i"
-
-            for (j in 0..numConfigurationsPerProject) {
-                buildType {
-                    id("subProj_bt_$i" + "_$j")
-                    name = "bt $i $j"
-                }
-            }
-        }
-    }
 }
