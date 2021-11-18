d143b0bc3c6dbdf0bde082119d87e31c76c90d6f
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-11-18 21:59:15 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,12 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 version = "2021.2"
 
 project {
+    params {
+        password("sec", "credentialsJSON:12315edf-7a6e-41f0-905f-7799fb08e9cd")
+        param("a", "a")
+        param("cd", "c")
+        param("b", "b")
+    }
     val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
     val numConfigurationsPerProject = Integer.parseInt(DslContext.getParameter("numConfigurationsPerProject"))
 
