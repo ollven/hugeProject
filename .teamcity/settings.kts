91492c2e4cd93fdd693f7421a6a1552f5f0cc358
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-11-18 22:00:50 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,7 +5,7 @@ version = "2021.2"
 
 project {
     params {
-        password("sec", "credentialsJSON:12315edf-7a6e-41f0-905f-7799fb08e9cd")
+        password("sec", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
         param("a", "a")
         param("cd", "c")
         param("b", "b")
