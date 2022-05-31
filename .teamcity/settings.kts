4a633cd6243dc1ad75cd3c08a083b85a820e1ce7
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-31 15:50:01 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -39,37 +39,7 @@ project {
         param("cd", "cjss")
         param("b", "b")
     }
-}
-
-object A : BuildType({
-    name = "a"
-
-    vcs {
-        root(HttpsGithubComGradleGradleGit)
-    }
-
-    features {
-        pullRequests {
-            vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
-            provider = github {
-                authType = token {
-                    token = "credentialsJSON:710498e1-c878-4b58-b5f1-cee3a258db6f"
-                }
-                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
-            }
-        }
-    }
-})
-
-object HttpsGithubComGradleGradleGit : GitVcsRoot({
-    name = "https://github.com/gradle/gradle.git"
-    url = "https://github.com/gradle/gradle.git"
-    branch = "refs/heads/master"
-})
-
-
-
-//val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
+    //val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
     val numProjects=1
     val numConfigurationsPerProject=500
     //val numConfigurationsPerProject = Integer.parseInt(DslContext.getParameter("numConfigurationsPerProject"))
@@ -103,3 +73,34 @@ object HttpsGithubComGradleGradleGit : GitVcsRoot({
             }
         }
     }
+}
+
+object A : BuildType({
+    name = "a"
+
+    vcs {
+        root(HttpsGithubComGradleGradleGit)
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:710498e1-c878-4b58-b5f1-cee3a258db6f"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
+})
+
+object HttpsGithubComGradleGradleGit : GitVcsRoot({
+    name = "https://github.com/gradle/gradle.git"
+    url = "https://github.com/gradle/gradle.git"
+    branch = "refs/heads/master"
+})
+
+
+
+
