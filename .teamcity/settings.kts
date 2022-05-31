0d89c4b2db33d7d25ea1cf56be4f11f653770c94
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-31 15:32:28 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,17 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.ui.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
+
+create(DslContext.projectId, GitVcsRoot({
+    id("HttpsGithubComGradleGradleGit")
+    name = "https://github.com/gradle/gradle.git"
+    url = "https://github.com/gradle/gradle.git"
+    branch = "refs/heads/master"
+}))
+
+
 
 version = "2021.2"
 
@@ -25,6 +37,18 @@ project {
                 buildType {
                     id("subProj_bt_$i" + "_$j")
                     name = "bt $i $j"
+                    features {
+    
+                pullRequests {
+                 vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
+                provider = github {
+                    authType = token {
+                  token = password("sec", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9ca")
+             }
+             filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+                 }
+    }
+}
                 }
             }
         }
