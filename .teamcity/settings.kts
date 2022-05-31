8882460e4fe6a762fb71a5108bf9cc24b6017a04
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-31 15:48:03 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -66,3 +66,40 @@ object HttpsGithubComGradleGradleGit : GitVcsRoot({
     url = "https://github.com/gradle/gradle.git"
     branch = "refs/heads/master"
 })
+
+
+
+//val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
+    val numProjects=1
+    val numConfigurationsPerProject=500
+    //val numConfigurationsPerProject = Integer.parseInt(DslContext.getParameter("numConfigurationsPerProject"))
+
+    for (i in 0..numProjects) {
+        subProject {
+            id("subProj_$i")
+            name = "subProj $i"
+
+            for (j in 0..numConfigurationsPerProject) {
+                buildType {
+                    id("subProj_bt_$i" + "_$j")
+                    name = "bt $i $j"
+                    
+    vcs {
+        root(HttpsGithubComGradleGradleGit)
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:710498e1-c878-4b58-b5f1-cee3a258db6f"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
+                }
+            }
+        }
+    }
