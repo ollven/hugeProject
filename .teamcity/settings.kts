2fa79092209e2152863b2f86ad20cdcd25d80cba
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-05-31 17:31:20 +0200

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,8 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildFeatures.PullRequests
+import jetbrains.buildServer.configs.kotlin.v2019_2.vcs.GitVcsRoot
+
 /*
 The settings script is an entry point for defining a TeamCity
 project hierarchy. The script should contain a single call to the
@@ -25,4 +28,79 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2021.2"
 
 project {
+
+    vcsRoot(HttpsGithubComGradleGradleGit)
+
+    buildType(A)
+
+    params {
+        password("sec", "credentialsJSON:22315edf-7a6e-41f0-905f-7799fb08e9cd")
+        param("a", "a")
+        param("cd", "cjss")
+        param("b", "b")
+    }
+    //val numProjects = Integer.parseInt(DslContext.getParameter("numProjects"))
+    val numProjects=1
+    val numConfigurationsPerProject=500
+    //val numConfigurationsPerProject = Integer.parseInt(DslContext.getParameter("numConfigurationsPerProject"))
+
+    for (i in 0..numProjects) {
+        subProject {
+            id("subProj_$i")
+            name = "subProj $i"
+
+            for (j in 0..numConfigurationsPerProject) {
+                buildType {
+                    id("subProj_bt_$i" + "_$j")
+                    name = "bt $i $j"
+                    
+    vcs {
+        root(HttpsGithubComGradleGradleGit)
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:710498e1-c878-4b58-b5f1-cee3a258db6f"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
+                }
+            }
+        }
+    }
 }
+
+object A : BuildType({
+    name = "a"
+
+    vcs {
+        root(HttpsGithubComGradleGradleGit)
+    }
+
+    features {
+        pullRequests {
+            vcsRootExtId = "${HttpsGithubComGradleGradleGit.id}"
+            provider = github {
+                authType = token {
+                    token = "credentialsJSON:710498e1-c878-4b58-b5f1-cee3a258db6f"
+                }
+                filterAuthorRole = PullRequests.GitHubRoleFilter.MEMBER
+            }
+        }
+    }
+})
+
+object HttpsGithubComGradleGradleGit : GitVcsRoot({
+    name = "https://github.com/gradle/gradle.git"
+    url = "https://github.com/gradle/gradle.git"
+    branch = "refs/heads/master"
+})
+
+
+
+
